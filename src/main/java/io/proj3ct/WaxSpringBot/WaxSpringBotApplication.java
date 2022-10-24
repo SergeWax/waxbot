31956834f9b5daaa866fe91aef18a1e2237cc8ae
sergewax <whitewaxbox@gmail.com>
2022-10-25 00:31:19 +0400

    first commit

---
+++ src/main/java/io/proj3ct/WaxSpringBot/WaxSpringBotApplication.java
@@ -0,0 +1,17 @@
+package io.proj3ct.WaxSpringBot;
+
+
+
+import io.proj3ct.WaxSpringBot.config.BotConfig;
+import io.proj3ct.WaxSpringBot.service.TelegramBot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaxSpringBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaxSpringBotApplication.class, args);
+	}
+
+}
---
+++ src/main/java/io/proj3ct/WaxSpringBot/service/TelegramBot.java
@@ -0,0 +1,64 @@
+package io.proj3ct.WaxSpringBot.service;
+
+import io.proj3ct.WaxSpringBot.config.BotConfig;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    final BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @SneakyThrows
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText) {
+                case "/start":
+                    StartCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+
+                default: sendMessage(chatId, "Sorry. Command was not recognized");
+            }
+        }
+    }
+    private void StartCommandReceived(long chatId, String name) {
+        String answer = "Hi, " + name + "!. " + "You have cool name!";
+
+        sendMessage(chatId, answer);
+    }
+    private void sendMessage(long chatId, String textToSend)  {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+
+        } catch (TelegramApiException e) {
+            System.out.println();
+        }
+
+    }
+}
